16acb7db48ddab935e94c7404a6e6704e7d383af
Aroldo <aroldobusiness@hotmail.com>
2024-03-05 21:20:21 -0400

    Primeiros passos API

--- Fundamentos-de-Front-End/003-pokedex/assets/js/main.js
+++ Fundamentos-de-Front-End/003-pokedex/assets/js/main.js
@@ -1,1 +1,17 @@
-console.log("Será que funfa?");
+//Variáveis
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon/?${offset}=0&${limit}=10`;
+
+//Requisição
+fetch(url)
+
+    //Recebe a resposta e converte para json usando Arrow Function
+    .then((response) => response.json())
+
+    //Recebe a resposta já convertida
+    .then((jsonBody) => console.log(jsonBody))
+
+    //Caso ocorra erro
+    .catch((error) => console.log(error));
+    
